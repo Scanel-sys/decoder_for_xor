8cb4108424b5bdcb1c2d567fbd6104fc512c6abb
Nickonson <nikitabilan2002@gmail.com>
2022-08-02 23:26:13 +0300

    enciphin is ready... i guess

---
+++ Source.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+using namespace std;
+
+void enciph(char *textFile, char *keyFile);
+
+int main()
+{
+    char textFile[] = "kursovaya/input.txt";
+    char keyFile[] =  "kursovaya/key.txt";
+    enciph(textFile, keyFile);
+
+    return 0;
+}
+
+void enciph(char *textFile, char *keyFile)
+{
+    FILE *text;
+    FILE *key;
+    FILE *output;
+    text = fopen(textFile, "r");
+    key = fopen(keyFile, "r");
+    output = fopen("kursovaya/output.txt", "w");
+
+    char textCh, keyCh, outCh;
+    while(!feof(text))
+    {
+        
+        textCh = fgetc(text);
+        keyCh = fgetc(key);
+        if(!feof(text))
+        {
+            if(feof(key))
+            {
+                fseek(key, 0, SEEK_SET);
+                keyCh = fgetc(key);
+            }
+            outCh = textCh ^ keyCh;
+            fputc(outCh, output);
+        }
+    }
+
+    fclose(text);
+    fclose(key);
+    fclose(output);
+}
